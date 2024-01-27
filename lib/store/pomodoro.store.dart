a5d2e51714212f15e74f7df58df4f5e5bdd5102a
@Souza83 <tec.psouza@gmail.com>
2024-01-27 18:35:22 -0300

    Pomodoro store #4.

--- lib/store/pomodoro.store.dart
+++ lib/store/pomodoro.store.dart
@@ -1,50 +1,65 @@
+import 'dart:async';
+
 import 'package:mobx/mobx.dart';
 
 part 'pomodoro.store.g.dart';
 
 class PomodoroStore = _PomodoroStore with _$PomodoroStore;
 
-enum TipoIntervalo { TRABALHO, DESCANSO}
+enum TipoIntervalo { TRABALHO, DESCANSO }
 
 abstract class _PomodoroStore with Store {
   @observable
   bool iniciado = false;
-  
+
   @observable
   int minutos = 2;
-  
+
   @observable
   int segundos = 0;
-   
+
   @observable
   int tempoTrabalho = 2;
-  
+
   @observable
   int tempoDescanso = 1;
 
   @observable
-  TipoIntervalo tipoIntervalo = TipoIntervalo.DESCANSO;
+  TipoIntervalo tipoIntervalo = TipoIntervalo.TRABALHO;
+
+  Timer? cronometro;
   
   @action
   void iniciar() {
     iniciado = true;
+    cronometro = Timer.periodic(Duration(milliseconds: 50), (timer) { 
+      if(minutos == 0 && segundos == 0) {
+        _trocarTipoIntervalo();
+      } else if(segundos == 0) {
+        segundos = 59;
+        minutos--;
+      } else {
+        segundos--;
+      }
+    });
   }
-  
+
   @action
   void parar() {
     iniciado = false;
+    cronometro?.cancel();
   }
-  
+
   @action
   void reiniciar() {
-    iniciado = false;
+    parar();
   }
-  
+
   @action
   void incrementarTempoTrabalho() {
     tempoTrabalho++;
   }
-  
+
   @action
   void decrementarTempoTrabalho() {
     tempoTrabalho--;
@@ -54,7 +69,7 @@ abstract class _PomodoroStore with Store {
   void incrementarTempoDescanso() {
     tempoDescanso++;
   }
-  
+
   @action
   void decrementarTempoDescanso() {
     tempoDescanso--;
@@ -63,8 +78,19 @@ abstract class _PomodoroStore with Store {
   bool estaTrabalhando() {
     return tipoIntervalo == TipoIntervalo.TRABALHO;
   }
-  
+
   bool estaDescansando() {
     return tipoIntervalo == TipoIntervalo.DESCANSO;
+  }
+
+  void _trocarTipoIntervalo() {
+    if (estaTrabalhando()) {
+      tipoIntervalo = TipoIntervalo.DESCANSO;
+      minutos = tempoDescanso;
+    } else {
+      tipoIntervalo = TipoIntervalo.TRABALHO;
+      minutos = tempoTrabalho;
+    }
+    segundos = 0;
   }
 }
